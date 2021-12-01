870bc4cc6505d40107a23e72591c77b444e24406
Hunter Nelson <90410647+huntnel@users.noreply.github.com>
2021-12-01 11:58:06 -0700

    Initial Framework

---
+++ Intex/OpiodPages/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from .views import indexPageView, analysis1PageView, analysis2PageView, analysis3PageView, analysisLandingView, drugSearchView, educationLandingView, predictorPageView, prescriberSearchView, recommenderPageView, searchLandingView 
+
+
+urlpatterns = [
+    path("", indexPageView, name = "index"),
+    path("analysis1/", indexPageView, name = "analysis1"),
+    path("analysis2/", indexPageView, name = "analysis2"),
+    path("analysis3/", indexPageView, name = "analysis3"),
+    path("analysislanding/", indexPageView, name = "analysislanding"),
+    path("drugsearch/", indexPageView, name = "drugsearch"),
+    path("educationlanding/", indexPageView, name = "educationlanding"),
+    path("predictor/", indexPageView, name = "predictor"),
+    path("prescribersearch/", indexPageView, name = "prescribersearch"),
+    path("recommender/", indexPageView, name = "recommender"),
+    path("searchlanding/", indexPageView, name = "searchlanding"),  
+]
---
+++ Intex/OpiodPages/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def indexPageView(request):
+    return render(request, 'OpiodPages/index.html')
+
+def analysis1PageView(request):
+    return render(request, 'OpiodPages/analysis1.html')
+
+def analysis2PageView(request):
+    return render(request, 'OpiodPages/analysis2.html')
+
+def analysis3PageView(request):
+    return render(request, 'OpiodPages/analysis3.html')
+
+def analysisLandingView(request):
+    return render(request, 'OpiodPages/analysislanding.html')
+
+def drugSearchView(request):
+    return render(request, 'OpiodPages/drugsearch.html')
+
+def educationLandingView(request):
+    return render(request, 'OpiodPages/educationlanding.html')
+
+def predictorPageView(request):
+    return render(request, 'OpiodPages/predictor.html')
+
+def prescriberSearchView(request):
+    return render(request, 'OpiodPages/prescribersearch.html')
+
+def recommenderPageView(request):
+    return render(request, 'OpiodPages/recommender.html')
+
+def searchLandingView(request):
+    return render(request, 'OpiodPages/searchlanding.html')
