4a844ffefa51e081b656e3acf8eb2351d9227513
Buscompany21 <buscobrian1@gmail.com>
2021-12-02 22:21:45 -0700

    lots of style stuff

--- Intex/OpiodPages/urls.py
+++ Intex/OpiodPages/urls.py
@@ -4,16 +4,16 @@ from .views import indexPageView, analysis1PageView, analysis2PageView, analysis
 
 urlpatterns = [
     path("", indexPageView, name = "index"),
-    path("analysis1/", indexPageView, name = "analysis1"),
-    path("analysis2/", indexPageView, name = "analysis2"),
-    path("analysis3/", indexPageView, name = "analysis3"),
-    path("analysislanding/", indexPageView, name = "analysislanding"),
-    path("drugsearch/", indexPageView, name = "drugsearch"),
+    path("analysis1/", analysis1PageView, name = "analysis1"),
+    path("analysis2/", analysis2PageView, name = "analysis2"),
+    path("analysis3/", analysis3PageView, name = "analysis3"),
+    path("analysislanding/", analysisLandingView, name = "analysislanding"),
+    path("drugsearch/", drugSearchView, name = "drugsearch"),
     path("educationlanding/", educationLandingView, name = "educationlanding"),
-    path("predictor/", indexPageView, name = "predictor"),
-    path("prescribersearch/", indexPageView, name = "prescribersearch"),
-    path("recommender/", indexPageView, name = "recommender"),
-    path("searchlanding/", indexPageView, name = "searchlanding"),  
+    path("predictor/", predictorPageView, name = "predictor"),
+    path("prescribersearch/", prescriberSearchView, name = "prescribersearch"),
+    path("recommender/", recommenderPageView, name = "recommender"),
+    path("searchlanding/", searchLandingView, name = "searchlanding"),  
     #Remove this later the one under this comment
     path("indexRename/", indexRenamePageView, name = "indexRename"),
 ]
