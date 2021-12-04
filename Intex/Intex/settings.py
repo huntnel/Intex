b96229fb6643174777b1c888aec10020891955d3
Hunter Nelson <90410647+huntnel@users.noreply.github.com>
2021-12-03 17:16:15 -0700

    sett

--- Intex/Intex/settings.py
+++ Intex/Intex/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'Intex.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'please',
+        'NAME': '',
         'USER': 'postgres',
         'PASSWORD' : 'admin',
         'HOST' : 'localhost',
