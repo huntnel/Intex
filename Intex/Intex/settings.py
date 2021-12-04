b64b91674b628f74c41334f7b8764e442bcb2564
cbenson22 <colepbenson22@gmail.com>
2021-12-03 17:22:00 -0700

    Analysis Page needs recommender

--- Intex/Intex/settings.py
+++ Intex/Intex/settings.py
@@ -74,16 +74,16 @@ WSGI_APPLICATION = 'Intex.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'please',
-        'USER': 'postgres',
-        'PASSWORD' : 'admin',
-        'HOST' : 'localhost',
-        'PORT' : 5432,
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'please',
+#         'USER': 'postgres',
+#         'PASSWORD' : 'admin',
+#         'HOST' : 'localhost',
+#         'PORT' : 5432,
+#     }
+# }
 
 
 # Password validation
